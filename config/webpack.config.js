f0d0ae7701b6c685b8998a02155ab73d29626a3d
dxiuxiu <1195811644@qq.com>
2021-02-22 15:52:28 +0800

    文件路劲别名配置

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 const fs = require('fs');
 const path = require('path');
@@ -334,6 +334,7 @@ module.exports = function (webpackEnv) {
           'scheduler/tracing': 'scheduler/tracing-profiling',
         }),
         ...(modules.webpackAliases || {}),
+        '@': path.resolve(__dirname, '../src'),
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
